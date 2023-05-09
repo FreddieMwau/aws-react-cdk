63636ca7ff6836931fc18b8ba05e97216e3dfcba
FreddieMwau <fredrickmutua12@gmail.com>
2023-05-09 17:43:36 +0300

    Created S3 bucket and Cloudfront distribution

---
+++ react-cdk/lib/react-cdk-stack.ts
@@ -0,0 +1,96 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ReactCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Add S3 s3.Bucket
+    const s3Site = new s3.Bucket(this, `CDK-React-UI`, {
+      // bucketName: 'cdk-reactapp-s3bucket',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      publicReadAccess: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      cors:[
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+          ],
+          allowedOrigins: ['http:localhost:3000'],
+          allowedHeaders: ['*'],
+        },
+      ],
+      lifecycleRules: [
+        {
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(1),
+          expiration: cdk.Duration.days(2),
+          transitions: [
+            {
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: cdk.Duration.days(2),
+            },
+          ],
+        },
+      ],
+    });
+    s3Site.grantRead(new iam.AccountRootPrincipal());
+
+    // Create a new CloudFront Distribution
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      `reactapp-cf-distribution`,
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: s3Site
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+                compress: true,
+                allowedMethods: cloudfront.CloudFrontAllowedMethods.ALL,
+                cachedMethods: cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD_OPTIONS,
+                forwardedValues: {
+                  queryString: true,
+                  cookies: {
+                    forward: "none"
+                  },
+                  headers: [
+                    "Access-Control-Request-Headers",
+                    "Access-Control-Request-Method",
+                  ]
+                }
+              }
+            ]
+          }
+        ],
+        comment: `reactapp - Cloudfront Distribution`,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+      },
+    );
+
+    // Setup Bucket Deployment to automatically deploy new assets and invalidate cache
+    new s3deploy.BucketDeployment(this, 'reactapp-s3bucketdeployment', {
+      sources: [s3deploy.Source.asset("../my-react-app/build")],
+      destinationBucket: s3Site,
+      distribution: distribution,
+      distributionPaths: ["/*"],
+    });
+
+    // Final Cloudfront URL
+    new cdk.CfnOutput(this, "Cloudfront URL", {
+      value: distribution.distributionDomainName
+    });
+
+  }
+}
